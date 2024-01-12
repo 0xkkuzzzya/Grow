e4ebaf5bd78b655505e90aae01a2ee80a55ca944
0xkkuzzzya <130092860+0xkkuzzzya@users.noreply.github.com>
2024-01-13 00:50:00 +0500

    feat:

--- src/components/Page/Earn/EarnFields/TokenField/TokenField.tsx
+++ src/components/Page/Earn/EarnFields/TokenField/TokenField.tsx
@@ -1,28 +1,43 @@
 import styled from "styled-components";
 import { EarnUSQToken } from "./Tokens/Tokens";
 import { USQARP } from "./ARP/ARP";
-import MuiAccordion, { AccordionProps } from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import { makeStyles, withStyles } from "@material-ui/core/styles";
+import { useAccordionEarn } from "../../../../../hooks/useAccordionEarn";
 
 
-const AccordionBlock = styled.div`
+const AccordionBlock = styled.div <{height: string}>`
     width: 100%;
+    max-height: ${props => props.height};
+    display: flex;
+    flex-direction: column;
+    border: 2px solid #eee;
+    border-radius: 15px;
+    transition: max-height .3s ease-in-out;
+    overflow: hidden;
+    padding-bottom: 10px;
+    cursor: pointer;
 `
 
-const ButtonsBlock = styled.div `
+const ButtonsBlock = styled.div`
     width: 100%;
-    max-height: 100%;
-    transition: max-height .3s ease-in-out;
+    
+    
     display: flex;
     justify-content: center;
     align-items: center;
     margin-bottom: 10px;
-    margin-top: 0;
+    margin-top: 10px;
+`
+
+const TokenFieldBlock = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 10px;
 `
 
 const EarnDepositButton = styled.button `
     width: 130px;
+    height: 100%;
     color: white;
     border: none;
     border-radius: 50px;
@@ -38,6 +53,7 @@ const EarnDepositButton = styled.button `
 
 const EarnWithdrawalButton = styled.button`
     width: 130px;
+    height: 100%;
     color: #3B9CFC;
     border: 2px solid #3B9CFC;
     border-radius: 50px;
@@ -51,48 +67,34 @@ const EarnWithdrawalButton = styled.button`
     transition: all .3s ease-in-out;
 `
 
-const Accordion = styled(MuiAccordion)(() => ({
-    
-  }));
-
-const TokenFieldBlock = styled(AccordionSummary)(({ theme }) => ({
-    
-  }));
-
-const useStylesSummary = makeStyles({
-    content: {
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        margin: '0px'
-    }
-  });
-
-
 export const USQField = () => {
-    const summary = useStylesSummary();
+
+    const [eAccordion, setEAccordion] = useAccordionEarn();
+
+    function openAccordion () {
+        if(eAccordion.active == false) {
+            setEAccordion({
+                active: true,
+                height: '170px',
+            })
+        } else if (eAccordion.active == true) {
+            setEAccordion({
+                active: false,
+                height: '55px',
+            })
+        }
+    }
+
     return(
-    <AccordionBlock>
-        <Accordion
-            TransitionProps={{ timeout: 200 }}
-            sx={{ 
-                boxShadow: 0,
-                border: '2px solid #eee'
-            }}>
-            <TokenFieldBlock
-            aria-controls="panel2-content"
-            classes={{ content: summary.content }}
-            sx={{ 
-                padding: '0',
-            }}
-            >
+        <AccordionBlock height={eAccordion.height} onClick={openAccordion}>
+            <TokenFieldBlock>
                 <EarnUSQToken/>
                 <USQARP/>
             </TokenFieldBlock>
-            <ButtonsBlock>
+            <ButtonsBlock >
                 <EarnDepositButton>Deposit</EarnDepositButton>
                 <EarnWithdrawalButton>Withdrawal</EarnWithdrawalButton>
             </ButtonsBlock>
-        </Accordion>
-      </AccordionBlock>
+        </AccordionBlock>
     )
 }
