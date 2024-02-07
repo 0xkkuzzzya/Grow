ed9c8821de4eef2856d73d52b7b76cb592c426ee
0xkkuzzzya <130092860+0xkkuzzzya@users.noreply.github.com>
2024-02-07 23:15:08 +0500

    feat:

--- src/components/Page/MyPage/MyPageBalance/MyPageBalance.tsx
+++ src/components/Page/MyPage/MyPageBalance/MyPageBalance.tsx
@@ -30,6 +30,14 @@ const AmountBlock = styled.div`
     margin-right: 20px;
 `
 
+const ContainerBlock = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+`
+
 
 export const MyPageBalance = () => {
     return(
@@ -40,6 +48,10 @@ export const MyPageBalance = () => {
                 <AmountBlock> <Text>Amount</Text> </AmountBlock>
             </InfoBlock>
             <TokenFieldBalance></TokenFieldBalance>
+            {/* <ContainerBlock >
+                <h1 style={{fontSize: "27px"}}>No tokens</h1>
+                <h3 style={{marginTop: "-15px"}}>Looks like you dont have any tokens yet.</h3>
+            </ContainerBlock> */}
         </BalanceBlock>
     )
 }
