7e48d10306c290b669ba880ef81d7f367f05267c
0xkkuzzzya <130092860+0xkkuzzzya@users.noreply.github.com>
2024-02-14 20:56:19 +0500

    fix:

--- src/components/Page/Earn/EarnPage/EarnContainer/EarnContainer.tsx
+++ src/components/Page/Earn/EarnPage/EarnContainer/EarnContainer.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { EarnHeader } from "../EarnHeader/EarnHeader";
 import { EarnSerach } from "../EarnSearch/EarnSearch";
 import { EarnFields } from "../EarnFields/EarnFields";
+import { useToggleTheme } from "../../../../../hooks/useToggleTheme";
 
 const ContainerBlock = styled.div`
     width: 500px;
@@ -14,12 +15,24 @@ const ContainerBlock = styled.div`
     align-items: center;
 `
 
+const ContainerBlockH = styled.div <{TextColor: string}>`
+    width: 100%;
+    text-align: center;
+    color: ${props => props.TextColor};
+`
+
 export const EarnContainer = () => {
+
+    const [theme, setTheme] = useToggleTheme()
+
     return(
         <ContainerBlock>
             <EarnHeader/>
             <EarnSerach/>
             <EarnFields/>
+            <ContainerBlockH TextColor={theme.TextColor}>
+                <h1 style={{fontSize: "27px"}}>No vaults</h1>
+            </ContainerBlockH>
         </ContainerBlock>
     )
 }
